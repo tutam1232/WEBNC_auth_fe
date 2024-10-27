4f4d0da8029a72548c8237bbf4b6c70e567d8b87
21120551 <damtutam@gmail.com>
2024-10-28 01:05:16 +0700

    fix url for calling api from prodution be

--- src/const/Url.js
+++ src/const/Url.js
@@ -1,9 +1,9 @@
 // Urls to be called in unplash api
 
 const Urls = {
-    LOGIN: "http://localhost:4000/user/login",
-    REGISTER: "http://localhost:4000/user/register",
-    HOME: "http://localhost:4000/home"
+    LOGIN: "https://webnc-auth-be.vercel.app/user/login",
+    REGISTER: "https://webnc-auth-be.vercel.app/user/register",
+    HOME: "https://webnc-auth-be.vercel.app/home"
 }
 
 export default Urls;
